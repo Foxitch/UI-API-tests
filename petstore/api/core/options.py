63e21b99cc74f4b04a387545994521bd99269b11
maksim.lazko <m.lazko@naveksoft.com>
2022-08-18 12:09:02 +0300

    renamed options added pytest.ini

--- petstore/api/core/options.py
+++ petstore/api/core/options.py
@@ -1,4 +1,4 @@
 def pytest_addoption(parser) -> None:
-    parser.addoption("--proto", action="store", default='https')
-    parser.addoption("--url", action="store", default='petstore.swagger.io')
+    parser.addoption("--proto_pet", action="store", default='https')
+    parser.addoption("--url_pet", action="store", default='petstore.swagger.io')
 
--- petstore/conftest.py
+++ petstore/conftest.py
@@ -5,6 +5,6 @@ from petstore.api.core.options import *
 @pytest.fixture(scope='function')
 def options(request) -> dict:
     dict_options = dict()
-    dict_options['proto'] = request.config.getoption('proto')
-    dict_options['url'] = request.config.getoption('url')
+    dict_options['proto'] = request.config.getoption('proto_pet')
+    dict_options['url'] = request.config.getoption('url_pet')
     return dict_options

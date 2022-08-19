8a56d0efc53997ccc235492ee8e8df7d4b03929a
maksim.lazko <m.lazko@naveksoft.com>
2022-08-19 10:03:41 +0300

    turn on headless mode

--- litecart/web_ui/core/fixture/base_application.py
+++ litecart/web_ui/core/fixture/base_application.py
@@ -19,8 +19,8 @@ class BaseApplication:
 
         options = Options()
         options.add_argument('--no-sandbox')
-        # options.add_argument('--headless')
-        # options.add_argument('--window-size=1920x1080')
+        options.add_argument('--headless')
+        options.add_argument('--window-size=1920x1080')
         options.add_argument('--start-maximized')
         options.add_argument('--disable-gpu')
         options.add_argument('--disable-dev-shm-usage')
